029011138c08ff194fa58d7d360de9f42004dfd3
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-05-11 02:52:07 +0400

    practice 6 and 5 modification

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ port = 5000
 
 app.use(express.json());
 //Q2
-const todos = [{ todo: " wake up", isCompleted: false }, { todo: "Eat Breakfast", isCompleted: false }];
+const todos = [{ todo: "wake up", isCompleted: false }, { todo: "Eat Breakfast", isCompleted: false }];
 
 //Q3
 app.get("/todos", (req, res) => {
@@ -40,24 +40,46 @@ app.post("/create/todo", (req, res) => {
 app.put("/update/todo/:name", (req, res) => {
     const name = req.params.name
 
-    let elem1 = {}
-
-    const found = todos.find((elem, i) => {
-        elem1 = elem
+    let i1 = 0
+    let found = todos.find((elem, i) => {
+        i1 = i
         return elem.todo === name
     })
 
     if (name) {
-        elem1 = { todo: req.body.todo, isCompleted: req.body.isCompleted }
-        todos.splice(0, 1, elem1)
+        found = { todo: req.body.todo, isCompleted: req.body.isCompleted }
+        todos.splice(i1, 1, found)
 
         res.status(200)
-        res.json(elem1)
+        res.json(found)
     } else {
         res.status(404);
         res.json("Todo not found");
     }
 })
+
+//Q6
+app.delete("/delete/todo/:name", (req, res) => {
+    const name = req.params.name
+
+    let i1 = 0
+    const found = todos.find((elem, i) => {
+        i1 = i
+        return elem.todo === name
+    })
+
+    if (found) {
+        todos.splice(i1, 1)
+
+        res.status(200)
+        res.json(found)
+    } else {
+        res.status(404);
+        res.json("Todo not found");
+    }
+})
+
+//Q7
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
